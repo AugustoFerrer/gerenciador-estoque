74df577b82dfd244637711e8a95bf5cc064582bb
AugustoFerrer <gutoprogramas05@gmail.com>
2026-07-01 11:19:30 -0300

    Implementando funções

--- estoque.py
+++ estoque.py
@@ -1,5 +1,18 @@
 estoque = {}
 
+def adicionar_item():
+    print("Adicionando item...")
+    produto = input("\nNome do produto: ")
+    qtd = input("\nQuantidade: ")
+    estoque[produto] = qtd
+
+def registrar_venda():
+    print("Registrando venda...")
+
+def listar_estoque():
+    print(estoque)
+
+
 while True:
     print("""
     ==============================
@@ -7,19 +20,23 @@ while True:
     ==============================
     [1] ADICIONAR ITEM
     [2] REGISTRAR VENDA
-    [3] SAIR
+    [3] LISTAR ESTOQUE
+    [4] SAIR
     ==============================
           """)
     
     opcao = input("Escolha uma opção: ")
 
     if opcao == "1":
-        print("adicionando item...")
+        adicionar_item()
 
     elif opcao == "2":
-        print("registrando venda...")
+        registrar_venda()
 
     elif opcao == "3":
+        listar_estoque()
+
+    elif opcao == "4":
         break
 
     else:
